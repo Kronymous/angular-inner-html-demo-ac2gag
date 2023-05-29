c1c0e5650060769147f02ab9ca639c24616c785a
Kronymous <97066478+Kronymous@users.noreply.github.com>
2023-05-29 00:23:29 -0600

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,47 +3,69 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  //*****************************************************/
+  //numeros
+  evento: number | undefined;
 
-//*****************************************************/
-//numeros
-evento: number | undefined
+  practica() {
+    var x = 10;
+    var y = 2;
+    var resultado = x + y;
 
-practica() {
+    this.evento = resultado;
+    console.log(this.evento);
+  }
+  //***************************************************/
+  //contador de numeros
 
-  var x = 10
-  var y = 2
-  var resultado = x + y
+  contador = 0;
 
-  this.evento = (resultado)
-  console.log(this.evento)
-}
-//***************************************************/
-//contador de numeros
+  incrementar() {
+    var x = 1;
+    this.contador += x;
+    console.log(this.contador);
+  }
+  //***************************************************/
+  //Cadena de texto
+  cadena: string | undefined;
 
-contador = 0
+  clave() {
+    this.cadena = 'hola';
+    console.log(this.cadena);
+  }
+  //***************************************************/
+  //imprimir etiqueta
+  cadena1: string | undefined;
 
-incrementar() {
-  var x = 1
-  this.contador += x
-  console.log(this.contador)
-}
-//***************************************************/
-//Cadena de texto
-cadena: string | undefined
+  clave2() {
+    this.cadena1 = '<h1>Hola<h1>';
+    console.log(this.cadena1);
+  }
+  //***************************************************/
+  //arreglo
 
-clave(){
-  this.cadena = "hola"
-  console.log(this.cadena)
-}
-//***************************************************/
-//imprimir etiqueta
-cadena1: string | undefined
+  nombres: string[] = ['melon', 'sandia', 'uva', 'platano'];
 
-clave2(){
-  this.cadena1 = '<h1>Hola<h1>'
-  console.log(this.cadena1)
-}
+  obtenerListaNombres(): string {
+    let listaHTML = '<ul>';
+    for (let i = 0; i < this.nombres.length; i++) {
+      listaHTML += '<li>' + i + ' - ' + this.nombres[i] + '</li>';
+    }
+    listaHTML += '</ul>';
+    return listaHTML;
+  }
+  //***************************************************/
+  //arreglo diferente
+  nombres1: string[] = ['carro', 'perro', 'silla', 'uva'];
+
+  array() {
+    const listaNombres = this.nombres1.map(
+      (nombre1, index) => `${index} - ${nombre1}`
+    );
+    return `<ul><li>${listaNombres.join('<li>')}</li></ul>`;
+  }
+  //**************************************************** */
 }
